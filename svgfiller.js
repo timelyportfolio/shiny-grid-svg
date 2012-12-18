a6eb12bcdb0955ecdbfa9296c016a63cd41f8c67
timelyportfolio <kent.russell@timelyportfolio.com>
2012-12-18 13:47:25 -0600

    just noticed I left commas in join()

--- svgfiller.js
+++ svgfiller.js
@@ -7,7 +7,7 @@ $.extend(networkOutputBinding, {
     renderValue: function(el, data) {
           //use join() to combine the array of strings in data that comprise the svg code
           //data is sent from shiny
-          $(el).html(data.join());      
+          $(el).html(data.join(''));      
     }
   });
   Shiny.outputBindings.register(networkOutputBinding, 'timelyportfolio.networkbinding');
