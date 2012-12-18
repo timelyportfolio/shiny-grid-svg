df7e3d388ce43e8318360407b17c15684f2c7959
timelyportfolio <kent.russell@timelyportfolio.com>
2012-12-18 12:21:22 -0600

    formatted IE disclaimer

--- ui.r
+++ ui.r
@@ -10,7 +10,7 @@ shinyUI(
       HTML("Motivated by the very fine work of Paul Murrell in this 
             <a href=\"http://www.stat.auckland.ac.nz/~paul/Reports/ggplotSlider/ggplotSlider.html\">Technical Report</a>
             and <a href=\"http://journal.r-project.org/current.html\">articles recently published in the R-Journal</a> I will show how to send
-            grid graphics as SVG to the browser with Shiny.  Does not work in IE.<br></br>"),
+            grid graphics as SVG to the browser with Shiny.<br></br>Does not work in IE.<br></br>"),
       selectInput("example", "Choose an example lattice chart (from lattice package documentation):", 
                   choices = c("xyplot", "dotplot", "barchart")),
       #submitButton("Make Grid SVG"),
